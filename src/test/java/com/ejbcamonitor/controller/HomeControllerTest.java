1f8691dfdfe6c91b619509daf501148b0db18b6f
Eduardo Andrade <edu.p.andrade@gmail.com>
2014-01-14 12:16:05 -0200

    @Ignored the unit test to check a given URL.

--- src/test/java/com/ejbcamonitor/controller/HomeControllerTest.java
+++ src/test/java/com/ejbcamonitor/controller/HomeControllerTest.java
@@ -3,15 +3,16 @@ package com.ejbcamonitor.controller;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.ejbcamonitor.controller.HomeController;
 import com.ejbcamonitor.model.CaStatus;
 
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/applicationContext.xml", "/applicationContext-test.xml"})
 public class HomeControllerTest {
@@ -22,6 +23,7 @@ public class HomeControllerTest {
     @Test
     public void testCheck() {
         
+        //TODO add your URL to test
         List<CaStatus> caStatusList = homeController.checkCaStatus(null);
         System.out.println(caStatusList);
         Assert.assertEquals(31, caStatusList.size());
